df3b32657c82390ce37d3619dd00c8e6315d0ee1
Bruno Caneo <brunocaneo3@gmail.com>
2022-05-09 12:35:18 -0300

    Adding Routes Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { SignIn } from "./pages/SignIn";
-import { Home } from "./pages/Home";
 import { GlobalStyle } from "./styles/global";
 import { AuthProvider } from "./contexts/AuthContext";
 import { InventoryProvider } from "./contexts/InventoryContext";
-import { SignUp } from "./pages/SignUp";
 import Modal from "react-modal";
-import { ReactNode } from "react";
-import { useAuth } from "./hooks/useAuth";
+import { RoutesComponent } from "./components/RoutesComponent";
 
 Modal.setAppElement("#root");
 
-type PrivateProps = {
-  children: ReactNode;
-};
 
 export function App() {
-  const Private = ({ children }: PrivateProps) => {
-    const { isLoggedIn } = useAuth();
-
-    if (!isLoggedIn) {
-      return <Navigate to="/" />
-    }
-
-    return (
-      <>
-        {children}
-      </>
-    )
-  }
 
   return (
     <InventoryProvider>
       <AuthProvider>
-        <BrowserRouter>
-          <Toaster />
-          <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="/dashboard" element={<Private><Home /></Private>} />
-            <Route path="/criarconta" element={<SignUp />} />
-          </Routes>
-          <GlobalStyle />
-        </BrowserRouter>
+        <Toaster />
+        <RoutesComponent />
+        <GlobalStyle />
       </AuthProvider>
     </InventoryProvider>
   );
